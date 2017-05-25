173abc678cb7c69dded8fbcba8087ee9b0172104
Anna Mikosz <annamikosz.lu@gmail.com>
2017-05-25 22:10:41 +0200

    Add comments to js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+//script.js 
+var name = prompt('Enter your name');
+alert('Hello, ' + name);   //There is a linear comment here
+console.log('Hello, ' + name);
+
+
+/*
+block comment
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+*/
