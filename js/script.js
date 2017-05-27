96c77e9bcb71b9f698fcd2881b08eb097d577134
Anna Mikosz <annamikosz.lu@gmail.com>
2017-05-27 13:18:28 +0200

    Exercises: conditional instructions JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var a = 2;
+var b = 3;
+var value = (a * a) + (2 * a * b) - (b * b);
+
+if (value > 0) {
+  console.log(' positive result = ' + value);
+} else if (value < 0) {
+  console.log(' negative result = ' + value);
+} else {
+  console.log(' result zero = ' + value);
+} 
